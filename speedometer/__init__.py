6a7aca750bd1e79057af2a19d3da3f6e9f07ef5a
15minutOdmora <liammislej@gmail.com>
2021-05-15 21:24:35 +0200

    Updated __init__.py

--- speedometer/__init__.py
+++ speedometer/__init__.py
@@ -1,5 +1,5 @@
 """
-Speedometer: A Python library for measuring the speed of objects captured in a still camera video.
+Speedometer: A Python package for measuring the speed of objects captured in a still camera video, uses cv2.
 """
 __title__ = "speedometer"
 __author__ = "Liam Mislej"
@@ -7,7 +7,6 @@ __license__ = "MIT Licence"
 __js__ = None
 __js_url__ = None
 
-from speedometer import object_tracking
-from speedometer import radar
-from speedometer import video
-
+from .object_tracking import ObjectTracking
+from .radar import Radar
+from .video import VideoPlayer
--- tests/object_tracking_test.py
+++ tests/object_tracking_test.py
@@ -1,6 +1,4 @@
-from speedometer.video import VideoPlayer
-# from speedometer.radar import Radar
-from speedometer.object_tracking import ObjectTracking
+from speedometer import VideoPlayer, ObjectTracking
 
 video = VideoPlayer(r"C:\Users\Liam\PycharmProjects\CarDetection\Video\\", fps=15)
 obt = ObjectTracking(video)
